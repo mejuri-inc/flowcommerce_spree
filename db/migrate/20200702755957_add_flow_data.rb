45d62c62295ae154783a28c894ca99856dc0010b
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-07-28 00:14:41 +0300

    [TEC-3339] Check column existence before creation in flow_data DB migration

--- db/migrate/20200702755957_add_flow_data.rb
+++ db/migrate/20200702755957_add_flow_data.rb
@@ -1,10 +1,10 @@
 class AddFlowData < ActiveRecord::Migration
   def up
-    add_column :spree_products, :flow_data, :jsonb, default: '{}'
-    add_column :spree_variants, :flow_data, :jsonb, default: '{}'
-    add_column :spree_orders, :flow_data, :jsonb, default: '{}'
-    add_column :spree_promotions, :flow_data, :jsonb, default: '{}'
-    add_column :spree_credit_cards, :flow_data, :jsonb, default: '{}'
+    add_column :spree_products, :flow_data, :jsonb, default: '{}' unless column_exists?(:spree_products, :flow_data)
+    add_column :spree_variants, :flow_data, :jsonb, default: '{}' unless column_exists?(:spree_variants, :flow_data)
+    add_column :spree_orders, :flow_data, :jsonb, default: '{}' unless column_exists?(:spree_orders, :flow_data)
+    add_column :spree_promotions, :flow_data, :jsonb, default: '{}' unless column_exists?(:spree_promotions, :flow_data)
+    add_column :spree_credit_cards, :flow_data, :jsonb, default: '{}' unless column_exists?(:spree_credit_cards, :flow_data)
   end
 
   def down
