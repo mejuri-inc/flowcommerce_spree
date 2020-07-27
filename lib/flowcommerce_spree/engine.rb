8e34be2054fa0fcdefc2ea2135f22b76299f8d09
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-07-28 00:56:16 +0300

    [TEC-3339] Require all gem's decorators in engine.rb

--- lib/flowcommerce_spree/engine.rb
+++ lib/flowcommerce_spree/engine.rb
@@ -9,5 +9,13 @@ module FlowcommerceSpree
 
       Flow::SimpleGateway.clear_zero_amount_payments = true
     end
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
+    config.to_prepare &method(:activate).to_proc
   end
 end
