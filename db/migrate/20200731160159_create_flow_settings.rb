6b8600c5d1e0bba361b911805f01e2ef4e4f30f3
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-08-28 00:55:40 +0300

    [TEC-3339] Changed CreateFlowSettings migration to prepare for STI
    Added indices and updated_at field.

--- db/migrate/20200731160159_create_flow_settings.rb
+++ db/migrate/20200731160159_create_flow_settings.rb
@@ -1,11 +1,22 @@
 class CreateFlowSettings < ActiveRecord::Migration
-  def change
+  def up
     unless table_exists?(:flow_settings)
       create_table :flow_settings do |t|
-        t.string :key
+        t.text :type
+        t.text :key
         t.jsonb :data, default: '{}'
-        t.datetime :created_at
+
+        t.timestamps
       end
     end
+
+    add_index :flow_settings, %i[id type key] unless index_exists?(:flow_settings, %i[id type key])
+    add_index :flow_settings, :data, using: :gin unless index_exists?(:flow_settings, :data)
+  end
+
+  def down
+    remove_index :flow_settings, :data if index_exists?(:flow_settings, :data)
+    remove_index :flow_settings, %i[id type key] if index_exists?(:flow_settings, %i[id type key])
+    drop_table :flow_settings if table_exists?(:flow_settings)
   end
 end
