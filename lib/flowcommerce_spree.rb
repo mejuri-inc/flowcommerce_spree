2b43a6240ce48f2445f0737cbeeae059d97de98e
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-09-01 17:02:56 +0300

    [TEC-3339] Fix require 'flowcommerce_spree/webhook' to require 'flowcommerce_spree/webhook_service'

--- lib/flowcommerce_spree.rb
+++ lib/flowcommerce_spree.rb
@@ -1,5 +1,5 @@
 require 'flowcommerce_spree/engine'
-require 'flowcommerce_spree/webhook'
+require 'flowcommerce_spree/webhook_service'
 require 'flow'
 require 'flow_api_refresh'
 require 'flow/simple_gateway'
