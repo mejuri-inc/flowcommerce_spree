caa2b05ff8a9b9049382c2e1294e794f74993419
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-09-01 18:29:38 +0300

    [TEC-3480] Extract params[:webhook], no need to parse request.body

--- app/controllers/flowcommerce_spree/webhooks_controller.rb
+++ app/controllers/flowcommerce_spree/webhooks_controller.rb
@@ -5,13 +5,13 @@ module FlowcommerceSpree
     # forward all incoming requests to Flow WebhookService object
     # /flow/event-target endpoint
     def handle_flow_web_hook_event
-      request_body_hash = Oj.load request.body.read
-      result = WebhookService.process(request_body_hash)
+      params_hash = params[:webhook]
+      result = WebhookService.process(params_hash)
     rescue StandardError => e
       result = { error: e.class.to_s, message: e.message, backtrace: e.backtrace }
     ensure
       response_status = result[:error] ? :unprocessable_entity : :ok
-      WebhookService::LOGGER.info(request_body_hash)
+      WebhookService::LOGGER.info(params_hash)
       render json: result.except(:backtrace), status: response_status
     end
   end
