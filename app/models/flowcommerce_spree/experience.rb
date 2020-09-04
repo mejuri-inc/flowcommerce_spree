b5a1abc72593fd843f759347c262d709171550c6
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-09-04 20:25:53 +0300

    [TEC-3471] Add validates_inclusion_of :country, in: Spree::Country.all.pluck(:iso3) to Experience model

--- app/models/flowcommerce_spree/experience.rb
+++ app/models/flowcommerce_spree/experience.rb
@@ -3,6 +3,8 @@ module FlowcommerceSpree
     store_accessor :data, :country, :currency, :delivered_duty, :language, :measurement_system,
                    :name, :position, :region, :settings, :status, :subcatalog
 
+    validates_inclusion_of :country, in: Spree::Country.all.pluck(:iso3)
+
     def upsert_data(received_experience)
       exp_hash = received_experience.is_a?(Hash) ? received_experience : received_experience.to_hash
 
