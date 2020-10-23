3917bb000cbbd68dba9f291a1c5cc5fe906fe1c3
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-10-23 23:15:42 +0300

    [TEC-3339] Use string path name instead of Rails.root.join in LOGGER definition
    LOGGER is a constant defined on class load, when there is no Rails.root yet defined.

--- lib/flowcommerce_spree.rb
+++ lib/flowcommerce_spree.rb
@@ -5,7 +5,7 @@ require 'flow_api_refresh'
 require 'flow/simple_gateway'
 
 module FlowcommerceSpree
-  LOG_PATH = defined?(Rails) && Dir.exist?('log') ? Rails.root.join('log/flow_io_webhook.log') : STDOUT
+  LOG_PATH = defined?(Rails) && Dir.exist?('log') ? 'log/flow_io_webhook.log' : STDOUT
   LOGGER = Logger.new(LOG_PATH, 3, 10_485_760)
 
   mattr_accessor :experience_associator
