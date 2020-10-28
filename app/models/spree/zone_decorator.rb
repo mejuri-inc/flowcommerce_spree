257ead7fbf60ec7edc7b7c2e3974fd050c019789
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-10-28 20:52:08 +0200

    [TEC-3339] Dynamically redefine Spree::Zone.available_currencies in engine initializer, instead of ugly way with AR callback

--- app/models/spree/zone_decorator.rb
+++ app/models/spree/zone_decorator.rb
@@ -2,22 +2,6 @@ module Spree
   Zone.class_eval do
     store_accessor :meta, :flow_data
 
-    after_initialize :redefine_available_currencies
-
-    def redefine_available_currencies
-      return if self.class.method_defined?(:available_currencies_redefined_by_flow?)
-
-      old_available_currencies = self.class.instance_method(:available_currencies)
-
-      self.class.__send__(:define_method, :available_currencies) do
-        (old_available_currencies.bind(self).() + [flow_data&.[]('currency')]).compact.uniq
-      end
-
-      self.class.__send__(:define_method, :available_currencies_redefined_by_flow?) do
-        true
-      end
-    end
-
     def import_flowcommerce(received_experience)
       self.flow_data = received_experience.is_a?(Hash) ? received_experience : received_experience.to_hash
 
