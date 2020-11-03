4a9c4abdfef934199b0d45fbd6a0bc8fc38f2c5a
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-11-03 21:54:38 +0200

    [TEC-3339] Add error handling to importing products from an experience
    Import all the experiences despite of their status.
    
    Fix product zone to be created and search for name as experience_key.titleize.
    
    Remove redundant FlowcommerceSpree:: namespacing on calls in FlowcommerceSpree::ImportExperiences.

--- app/services/flowcommerce_spree/import_experiences.rb
+++ app/services/flowcommerce_spree/import_experiences.rb
@@ -6,63 +6,65 @@ module FlowcommerceSpree
 
     def run
       # we have to log start, so that another process does not start while this one is running
-      FlowcommerceSpree::ApiRefresh.log_refresh!
+      ApiRefresh.log_refresh!
 
       total = 0
       experiences = @client.experiences.get(@organization)
 
       experiences.each do |experience|
         experience_key = experience.key
-        zone = Spree::Zones::Product.find_or_initialize_by(name: experience_key)
+        zone = Spree::Zones::Product.find_or_initialize_by(name: experience_key.titleize)
         zone.import_flowcommerce(experience)
 
-        if experience.status.value == 'active'
-          page_size  = 100
-          offset     = 0
-          items      = []
+        page_size  = 100
+        offset     = 0
+        items      = []
 
-          while offset == 0 || items.length == 100
-            # show current list size
-            puts "\nGetting items: #{experience_key.green}, rows #{offset} - #{offset + page_size}"
+        while offset == 0 || items.length == 100
+          # show current list size
+          puts "\nGetting items: #{experience_key.green}, rows #{offset} - #{offset + page_size}"
 
-            items = @client.experiences.get_items(
-              @organization, experience: experience_key, limit: page_size, offset: offset
-            )
+          begin
+            items = @client.experiences
+                           .get_items(@organization, experience: experience_key, limit: page_size, offset: offset)
+          rescue Io::Flow::V0::HttpClient::PreconditionException => e
+            puts "#{e.message}"
+            break
+          end
 
-            offset += page_size
+          offset += page_size
 
-            items.each do |item|
-              total += 1
-              item_hash = item.to_hash
-              next unless (variant = Spree::Variant.find_by(sku: item_hash.delete(:number)))
+          items.each do |item|
+            total += 1
+            item_hash = item.to_hash
+            next unless (variant = Spree::Variant.find_by(sku: item_hash.delete(:number)))
 
-              # if item is not included, mark it in product as excluded regardless if excluded or restricted
-              status_in_experience = item_hash.dig(:local, :status)
-              unless status_in_experience == 'included'
-                print "[#{status_in_experience.red}]:"
-                if (product = variant.product)
-                  product.flow_data ||= {}
-                  product.flow_data["#{experience_key}.excluded"] = 1
-                  product.update_column(:meta, product.meta.to_json)
-                end
+            # if item is not included, mark it in product as excluded regardless if excluded or restricted
+            status_in_experience = item_hash.dig(:local, :status)
+            unless status_in_experience == 'included'
+              print "[#{status_in_experience.red}]:"
+              if (product = variant.product)
+                product.flow_data ||= {}
+                product.flow_data["#{experience_key}.excluded"] = 1
+                product.update_column(:meta, product.meta.to_json)
               end
-
-              variant.flow_import_item(item_hash, experience_key: experience_key)
-
-              print "#{variant.sku}, "
             end
+
+            variant.flow_import_item(item_hash, experience_key: experience_key)
+
+            print "#{variant.sku}, "
           end
         end
       end
 
       # Log sync end time
-      FlowcommerceSpree::ApiRefresh.log_refresh!(has_ended: true)
+      ApiRefresh.log_refresh!(has_ended: true)
       total
     end
 
     private
 
-    def initialize(client: FlowcommerceSpree::CLIENT, organization: FlowcommerceSpree::ORGANIZATION)
+    def initialize(client: CLIENT, organization: ORGANIZATION)
       @client = client
       @organization = organization
     end
