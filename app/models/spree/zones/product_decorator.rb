111032c5387cc4fe22b41aa084bd51e8360e2530
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-11-04 21:05:21 +0200

    [TEC-3339] Rename Spree::Zones::Product.import_flowcommerce to correct `store_flow_io_data`
    Add error handling to Spree::Zones::Product.store_flow_io_data and to ImportExperiences SO.

--- app/models/spree/zones/product_decorator.rb
+++ app/models/spree/zones/product_decorator.rb
@@ -20,17 +20,17 @@ module Spree
         false if client.instance_variable_get(:@http_handler).http_client.error
       end
 
-      def import_flowcommerce(received_experience, logger: FlowcommerceSpree.logger)
+      def store_flow_io_data(received_experience, logger: FlowcommerceSpree.logger)
         self.flow_data = received_experience.is_a?(Hash) ? received_experience : received_experience.to_hash
         self.status = flow_data['status']
 
-        if new_record?
-          update_attributes(meta: meta, status: status)
+        if new_record? && update_attributes(meta: meta, status: status, kind: 'country')
           logger.info "\nNew flow.io experience imported as product zone: #{name}"
-        else
-          update_columns(meta: meta.to_json, status: status)
+        elsif update_columns(meta: meta.to_json, status: status, kind: 'country')
           logger.info "\nProduct zone `#{name}` has been updated from flow.io"
         end
+
+        self
       end
     end
   end
