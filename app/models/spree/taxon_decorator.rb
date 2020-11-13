98b4062a23a4c067b03b9b214e4343b8b86f0bbd
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-11-13 15:28:34 +0200

    [TEC-3759] Made taxon_decorator methods `products_by_zone` and `products_by_experience` aware of the zone's sku_suffix to fetch only the matching products

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -2,15 +2,16 @@ module Spree
   Taxon.class_eval do
     def products_by_zone(product_zone)
       flow_experience_key = product_zone.flow_data&.[]('key')
+      sku_regex = product_zone.sku_regex
 
       if flow_experience_key.present?
-        products_by_experience(flow_experience_key)
+        products_by_experience(product_zone, sku_regex)
       else
-        products
+        products.joins(:master).where('spree_variants.sku ~ ?', sku_regex)
       end
     end
 
-    def products_by_experience(flow_experience_key)
+    def products_by_experience(flow_experience_key, sku_regex)
       # To make the following query return a distinct array of products, raw SQL had to be used:
       # object.products.joins(:variants).where(
       #   "spree_variants.meta -> 'flow_data' -> 'exp' ->> '#{flow_experience_key}' IS NOT NULL"
@@ -18,7 +19,8 @@ module Spree
       query = <<~SQL
           SELECT DISTINCT spree_products.* FROM spree_products 
             INNER JOIN spree_variants ON spree_variants.product_id = spree_products.id AND 
-              spree_variants.is_master = 'f' AND spree_variants.deleted_at IS NULL 
+              spree_variants.is_master = 'f' AND spree_variants.deleted_at IS NULL AND 
+              (spree_variants.sku ~ '#{sku_regex}')
             INNER JOIN (
               SELECT spree_products_taxons.*, spree_products_taxons.position as position from spree_products_taxons
                 ORDER BY position ASC
