1985020d7465cc06781a4bb2c7664fe8f9273a3e
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-11-27 23:55:58 +0200

    [TEC-3339] Move the FlowcommerceSpree constants and ENV['FLOW_TOKEN'] to the initializer

--- lib/flowcommerce_spree/engine.rb
+++ lib/flowcommerce_spree/engine.rb
@@ -1,11 +1,4 @@
 module FlowcommerceSpree
-  ORGANIZATION = ENV.fetch('FLOW_ORGANIZATION', 'flow.io')
-  BASE_COUNTRY = ENV.fetch('FLOW_BASE_COUNTRY', 'USA')
-  API_KEY = ENV.fetch('FLOW_API_KEY', 'test_key')
-
-  # For compatibility with flowcommerce gem
-  ENV['FLOW_TOKEN'] = API_KEY
-
   class Engine < ::Rails::Engine
     require 'spree/core'
     isolate_namespace FlowcommerceSpree
