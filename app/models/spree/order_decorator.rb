b21dafebf9e49b4d4f67dd6ad204f814d691dbff
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-12-14 04:37:43 +0200

    [TEC-3473] Add `before_save :sync_to_flow_io` to Spree::Order decorator
    Fixed FlowcommerceSpree::OrderSync service object to work.

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,31 +1,26 @@
-# Flow (2017)
+# frozen_string_literal: true
+
 # `:display_total` modifications to display total prices beside Spree default. Example: https://i.imgur.com/7v2ix2G.png
-# Added flow specific methods to Spree::Order
 module Spree
-  Order.class_eval do
+  # Added flow specific methods to Spree::Order
+  Order.class_eval do # rubocop:disable Metrics/BlockLength
     serialize :meta, ActiveRecord::Coders::JSON.new(symbolize_keys: true)
 
     store_accessor :meta, :flow_data
 
-    state_machine.before_transition from: :cart, do: :sync_to_flow_io_if_needed
+    before_save :sync_to_flow_io, if: lambda { |order|
+      order.zone&.flow_io_active_experience? && order.state == 'cart' && order.line_items.exists?
+    }
 
-    def sync_to_flow_io_if_needed
-      return unless zone&.flow_io_active_experience?
+    def sync_to_flow_io
+      flow_io_order = FlowcommerceSpree::OrderSync.new(order: self)
+      flow_io_order.synchronize!
     end
 
     def display_total
-      price = Flow.format_default_price total
-      price += ' (%s)' % flow_total if flow_order
+      price = FlowcommerceSpree::Api.format_default_price total
+      price += " (#{flow_total})" if flow_order
       price.html_safe
-    end
-
-    # we now use Spree number as Flow number, but I will leave this here for now
-    def flow_number
-      return self[:flow_number] unless self[:flow_number].blank?
-
-      return unless id
-
-      number
     end
 
     def flow_order
@@ -46,12 +41,12 @@ module Spree
 
                  total ? item['total']['label'] : item['price']['label']
                else
-                 Flow.format_default_price(line_item.price * (total ? line_item.quantity : 1))
+                 FlowcommerceSpree::Api.format_default_price(line_item.price * (total ? line_item.quantity : 1))
                end
 
       # add line item promo
       # promo_total, adjustment_total
-      result += ' (%s)' % Flow.format_default_price(line_item.promo_total) if line_item.promo_total > 0
+      result += ' (%s)' % FlowcommerceSpree::Api.format_default_price(line_item.promo_total) if line_item.promo_total > 0
 
       result
     end
@@ -73,18 +68,18 @@ module Spree
           prices.push price_model.new(price['name'], price['label'])
         end
       else
-        price_elements = [:item_total, :adjustment_total, :included_tax_total, :additional_tax_total, :tax_total, :shipment_total, :promo_total]
+        price_elements = %i[item_total adjustment_total included_tax_total additional_tax_total tax_total shipment_total promo_total]
         price_elements.each do |el|
           price = send(el)
           if price > 0
-            label = Flow.format_default_price price
+            label = FlowcommerceSpree::Api.format_default_price price
             prices.push price_model.new(el.to_s.humanize.capitalize, label)
           end
         end
 
         # discount is applied and we allways show it in default currency
         if adjustment_total != 0
-          formated_discounted_price = Flow.format_default_price adjustment_total
+          formated_discounted_price = FlowcommerceSpree::Api.format_default_price adjustment_total
           prices.push price_model.new('Discount', formated_discounted_price)
         end
       end
@@ -117,16 +112,16 @@ module Spree
       self.flow_data = (flow_data || {}).merge!('exp' => zone.flow_io_experience)
     end
 
-    # clear invalid zero amount payments. Solidsus bug?
+    # clear invalid zero amount payments. Solidus bug?
     def clear_zero_amount_payments!
       # class attribute that can be set to true
       return unless Flow::Order.clear_zero_amount_payments
 
-      payments.where(amount:0, state: ['invalid', 'processing', 'pending']).map(&:destroy)
+      payments.where(amount: 0, state: %w[invalid processing pending]).map(&:destroy)
     end
 
     def flow_order_authorized?
-      flow_data && flow_data['authorization'] ? true : false
+      flow_data&.[]('authorization') ? true : false
     end
 
     def flow_order_captured?
--- app/services/flowcommerce_spree/order_sync.rb
+++ app/services/flowcommerce_spree/order_sync.rb
@@ -1,55 +1,43 @@
-# Flow.io (2017)
-
-# represents flow.io order
-# for easy intgration we pass current
-# - flow experirnce
-# - spree order
-# - current customer, presetnt as  @current_spree_user controller instance variable
-#
-# example:
-#  flow_order = Flow::Order.new    # init flow-order object
-#    order: Spree::Order.last,
-#    experience: @flow_session.experience
-#    customer: Spree::User.last
-#  fo.build_flow_request           # builds json body to be posted to flow api
-#  fo.synchronize!                 # sends order to flow
+# frozen_string_literal: true
 
 module FlowcommerceSpree
+  # represents flow.io order syncing service
+  # for easy integration we are currently passing:
+  # - flow experience
+  # - spree order
+  # - current customer, presetnt as  @current_spree_user controller instance variable
+  #
+  # example:
+  #  flow_order = FlowcommerceSpree::OrderSync.new    # init flow-order object
+  #    order: Spree::Order.last,
+  #    experience: @flow_session.experience
+  #    customer: Spree::User.last
+  #  flow_order.build_flow_request           # builds json body to be posted to flow.io api
+  #  flow_order.synchronize!                 # sends order to flow
   class OrderSync
-    FLOW_CENTER = 'default' unless defined?(::FlowcommerceSpree::OrderSync::FLOW_CENTER)
+    FLOW_CENTER = 'default'
 
-    attr_reader     :response
-    attr_reader     :order
-    attr_reader     :customer
-    attr_reader     :body
+    attr_reader :order, :response
 
     class << self
-      def clear_cache order
+      def clear_cache(order)
         return unless order.flow_data['order']
+
         order.flow_data.delete('order')
         order.update_column :flow_data, order.flow_data.dup
       end
     end
 
-    ###
+    def initialize(order:)
+      raise(ArgumentError, 'Experience not defined or not active') unless order.zone&.flow_io_active_experience?
 
-    def initialize order:, experience: nil, customer: nil
-      # when sending email, we do not have experience defined
-      unless experience
-        if order.flow_order
-          experience = FlowcommerceSpree::ExperienceService.get(order.flow_order['experience']['key'])
-        else
-          raise(ArgumentError, 'Experience not defined and not found in flow data')
-        end
-      end
-
-      @experience = experience
+      @experience = order.flow_experience_key
       @order      = order
-      @customer   = customer
+      @customer   = order.user_id
       @items      = []
     end
 
-    # helper method to send complete order from spreee to flow
+    # helper method to send complete order from Spree to flow.io
     def synchronize!
       sync_body!
       check_state!
@@ -66,11 +54,11 @@ module FlowcommerceSpree
     end
 
     def error?
-      @response && @response['code'] && @response['messages'] ? true : false
+      @response&.[]('code') && @response&.[]('messages') ? true : false
     end
 
     def delivery
-      deliveries.select{ |el| el[:active] }.first
+      deliveries.select { |el| el[:active] }.first
     end
 
     # delivery methods are defined in flow console
@@ -89,16 +77,14 @@ module FlowcommerceSpree
 
         selection_id = opts['id']
 
-        {
-          id:    selection_id,
+        { id: selection_id,
           price: { label: opts['price']['label'] },
           active: @order.flow_order['selections'].include?(selection_id),
-          name: name
-        }
+          name: name }
       end.to_a
 
       # make first one active unless we have active element
-      delivery_list.first[:active] = true unless delivery_list.select{ |el| el[:active] }.first
+      delivery_list.first[:active] = true unless delivery_list.select { |el| el[:active] }.first
 
       delivery_list
     end
@@ -116,82 +102,66 @@ module FlowcommerceSpree
 
     # if customer is defined, add customer info
     # it is possible to have order in Spree without customer info (new guest session)
-    def add_customer opts
+    def add_customer
       return unless @customer
 
       address = @customer.ship_address
       # address = nil
       if address
-        opts[:customer] = {
-          name:   {
-            first: address.firstname,
-            last: address.lastname
-          },
-          email:  @customer.email,
-          number: @customer.flow_number,
-          phone:  address.phone
-        }
+        @body[:customer] = { name: { first: address.firstname,
+                                     last: address.lastname },
+                             email: @customer.email,
+                             number: @customer.flow_number,
+                             phone: address.phone }
 
         streets = []
         streets.push address.address1 unless address.address1.blank?
         streets.push address.address2 unless address.address2.blank?
 
-        opts[:destination] = {
-          streets:  streets,
-          city:     address.city,
-          province: address.state_name,
-          postal:   address.zipcode,
-          country: (address.country.iso3 rescue 'USA'),
-          contact: opts[:customer]
-        }
+        @body[:destination] = { streets: streets,
+                                city: address.city,
+                                province: address.state_name,
+                                postal: address.zipcode,
+                                country: (address.country.iso3 rescue 'USA'),
+                                contact: @body[:customer] }
 
-        opts[:destination].delete_if { |k,v| v.nil? }
+        @body[:destination].delete_if { |_k, v| v.nil? }
       end
 
-      opts
+      @body
     end
 
     # builds object that can be sent to api.flow.io to sync order data
     def build_flow_request
-      @order.line_items.each do |line_item|
-        add_item line_item
-      end
+      @order.line_items.each { |line_item| add_item(line_item) }
 
-      flow_number = @order.flow_number
+      @opts = {}
+      @opts[:organization] = FlowcommerceSpree::ORGANIZATION
+      @opts[:experience]   = @experience
+      @opts[:expand]       = 'experience'
 
-      opts = {}
-      opts[:organization] = FlowcommerceSpree::ORGANIZATION
-      opts[:experience]   = @experience.key
-      opts[:expand]       = 'experience'
+      @body = { items: @items, number: @order.number }
 
-      body = {}
-      body = {
-        items:  @items,
-        number: flow_number
-      }
-
-      add_customer body if @customer
+      add_customer if @customer
 
       # if defined, add selection (delivery options) and delivered_duty from flow_data
-      body[:selections]     = [@order.flow_data['selection']]    if @order.flow_data['selection']
-      body[:delivered_duty] = @order.flow_data['delivered_duty'] if @order.flow_data['delivered_duty']
+      @body[:selections] = [@order.flow_data['selection']] if @order.flow_data['selection']
+      @body[:delivered_duty] = @order.flow_data['delivered_duty'] if @order.flow_data['delivered_duty']
 
       # discount on full order is applied
       if @order.adjustment_total != 0
-        body[:discount] = {
-          amount: @order.adjustment_total,
-          currency: @order.currency
-        }
+        @body[:discount] = { amount: @order.adjustment_total,
+                             currency: @order.currency }
       end
 
       # calculate digest body and cache it
-      @digest = Digest::SHA1.hexdigest(opts.to_json + body.to_json)
+      @digest = Digest::SHA1.hexdigest(@opts.to_json + @body.to_json)
 
-      [opts, body]
+      [@opts, @body]
     end
 
     def sync_body!
-      opts, @body = build_flow_request
+      @opts, @body = build_flow_request if @body.blank?
 
       @use_get = false
 
@@ -209,14 +179,14 @@ module FlowcommerceSpree
       else
         # replace when fixed integer error
         # @body[:items].map! { |item| ::Io::Flow::V0::Models::LineItemForm.new(item) }
-        # opts[:experience] = @experience.key
+        # @opts[:experience] = @experience.key
         # order_put_form = ::Io::Flow::V0::Models::OrderPutForm.new(@body)
-        # r FlowcommerceSpree.client.orders.put_by_number(FlowcommerceSpree::ORGANIZATION, @order.flow_number, order_put_form, opts)
+        # r FlowcommerceSpree.client.orders.put_by_number(FlowcommerceSpree::ORGANIZATION, @order.number, order_put_form, @opts)
 
         # cache last order/put for debug purposes
-        FlowSettings.set 'flow-order-put-body-%s' % @body[:number], @body.to_json
+        # FlowSettings.set 'flow-order-put-body-%s' % @body[:number], @body.to_json
 
-        @response = FlowcommerceSpree::Api.run :put, '/:organization/orders/%s' % @body[:number], opts, @body
+        @response = FlowcommerceSpree::Api.run :put, "/:organization/orders/#{@body[:number]}", @opts, @body
       end
     end
 
@@ -233,20 +203,16 @@ module FlowcommerceSpree
       @order.flow_finalize! if @order.flow_order_authorized? && @order.state != 'complete'
     end
 
-    def add_item line_item
+    def add_item(line_item)
       variant    = line_item.variant
-      price_root = variant.flow_data['exp'][@experience.key]['prices'][0] rescue {}
+      price_root = variant.flow_data.dig('exp', @experience, 'prices')[0] || {}
 
       # create flow order line item
-      item = {
-        center: FLOW_CENTER,
-        number: variant.id.to_s,
-        quantity: line_item.quantity,
-        price: {
-          amount:   price_root['amount']   || variant.cost_price,
-          currency: price_root['currency'] || variant.cost_currency
-        }
-      }
+      item = { center: FLOW_CENTER,
+               number: variant.sku,
+               quantity: line_item.quantity,
+               price: { amount: price_root['amount'] || variant.cost_price,
+                        currency: price_root['currency'] || variant.cost_currency } }
 
       @items.push item
     end
@@ -269,8 +235,7 @@ module FlowcommerceSpree
         @order.flow_data['order']  = @response.to_hash
       end
 
-      @order.save
+      @order.update_column(:meta, @order.meta.to_json)
     end
   end
 end
-
