e460c1ff82c5fbd291d19d72dded8655942f8809
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-12-15 22:09:00 +0200

    [TEC-3473] Fix flow_io_order_helper_decorator.adjust_zone_and_ip to @current_order.save if @current_order.new_record? in any cases

--- app/helpers/spree/core/controller_helpers/flow_io_order_helper_decorator.rb
+++ app/helpers/spree/core/controller_helpers/flow_io_order_helper_decorator.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-# Spree::Core::ControllerHelpers::Order decorator to inject Flow.io Experience into the order, if such an Experience
-# is present in the Order's zone
 module Spree
   module Core
     module ControllerHelpers
+      # Spree::Core::ControllerHelpers::Order decorator to inject Flow.io Experience into the order, if such an
+      # Experience is present in the Order's zone
       module FlowIoOrderHelperDecorator
         private
 
@@ -15,10 +15,10 @@ module Spree
 
           if @current_order.zone&.flow_io_active_experience? && @current_order.flow_experience_key.nil?
             @current_order.flow_io_experience_from_zone
-            return @current_order.save if @current_order.new_record?
-
             update_meta = true
           end
+
+          return @current_order.save if @current_order.new_record?
 
           # :meta is a jsonb column costly to update every time, especially with all the flow.io data, that's why
           # here it is updated only if no zone_id there was inside :meta
