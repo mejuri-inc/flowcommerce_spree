da9b25a514fa1ab49f2113ba400903f0528eac33
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2020-12-17 00:34:55 +0200

    [TEC-3471] Refactored ImportExperienceItems to receive the zone instead of experience_key and to associate variant's product with the zone, if at least a variant is included in zone's flow.io experience

--- app/services/flowcommerce_spree/import_experiences.rb
+++ app/services/flowcommerce_spree/import_experiences.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 module FlowcommerceSpree
+  # A service object to import the data for of flow.io Experience into Spree::Zones::Product
   class ImportExperiences
     def self.run(client: FlowcommerceSpree.client, organization: ORGANIZATION, with_items: nil, refresher: nil)
       new(client: client, organization: organization, with_items: with_items, refresher: refresher).run
@@ -8,22 +11,18 @@ module FlowcommerceSpree
       # we have to log start, so that another process does not start while this one is running
       @refresher.log_refresh!
 
-      total = 0
-      experiences = @client.experiences.get(@organization)
-
-      experiences.each do |experience|
+      @client.experiences.get(@organization).each do |experience|
         experience_key = experience.key
         zone = Spree::Zones::Product.find_or_initialize_by(name: experience_key.titleize)
         zone.store_flow_io_data(experience, logger: @refresher.logger)
 
         next @refresher.logger.info "Error: storing flow.io experience #{experience_key}" if zone.errors.any?
 
-        ImportExperienceItems.run(experience_key, client: @client) if @with_items
+        ImportExperienceItems.run(zone, client: @client) if @with_items
       end
 
       # Log sync end time
       @refresher.log_refresh!(has_ended: true)
-      total
     end
 
     private
