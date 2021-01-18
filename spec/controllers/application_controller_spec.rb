7e7909ffab895a8d4ab12c8b5d493700c7b40110
Sebastian <delucasd@gmail.com>
2021-01-18 10:46:21 -0300

    [TEC-3657] Renaming application_controller specs

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'spec_helper'
 require 'shared_examples/current_zone_loader'
 
 RSpec.describe ApplicationController, type: :controller do
