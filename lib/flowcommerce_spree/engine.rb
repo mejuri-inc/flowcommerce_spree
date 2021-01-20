69d29ea56860fcb6e0647b2883844889f4193176
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2021-01-20 19:04:29 +0200

    [TEC-3473] Fix FlowcommerceSpree constants' definitions to be performed in the engine's hooks, because ENV.fetch on class definition doesn't work

--- lib/flowcommerce_spree/engine.rb
+++ lib/flowcommerce_spree/engine.rb
@@ -1,27 +1,25 @@
 # frozen_string_literal: true
 
 module FlowcommerceSpree
-  ORGANIZATION = ENV.fetch('FLOW_ORGANIZATION', 'flow.io')
-  BASE_COUNTRY = ENV.fetch('FLOW_BASE_COUNTRY', 'USA')
-  API_KEY = ENV.fetch('FLOW_TOKEN', 'test_key')
-  MOUNT_PATH = ENV.fetch('FLOW_MOUNT_PATH', '/flow')
-
   class Engine < ::Rails::Engine
     require 'spree/core'
     isolate_namespace FlowcommerceSpree
 
     config.before_initialize do
+      FlowcommerceSpree::ORGANIZATION = ENV.fetch('FLOW_ORGANIZATION', 'flow.io')
+      FlowcommerceSpree::BASE_COUNTRY = ENV.fetch('FLOW_BASE_COUNTRY', 'USA')
+      FlowcommerceSpree::API_KEY = ENV.fetch('FLOW_TOKEN', 'test_key')
+
       FlowcommerceSpree::Config = FlowcommerceSpree::Settings.new
     end
 
     config.flowcommerce_spree = ActiveSupport::OrderedOptions.new
-    config.flowcommerce_spree[:mounted_path] = FlowcommerceSpree::MOUNT_PATH
 
     initializer 'flowcommerce_spree.configuration' do |app|
-      if app.config.flowcommerce_spree[:mounted_path]
-        app.routes.append do
-          mount FlowcommerceSpree::Engine => app.config.flowcommerce_spree[:mounted_path]
-        end
+      app.config.flowcommerce_spree[:mounted_path] = ENV.fetch('FLOW_MOUNT_PATH', '/flow')
+
+      app.routes.append do
+        mount FlowcommerceSpree::Engine => app.config.flowcommerce_spree[:mounted_path]
       end
     end
 
