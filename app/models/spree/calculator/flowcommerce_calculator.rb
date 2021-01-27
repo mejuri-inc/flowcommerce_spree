dd28d81bf45f0ac9e89c9ff5e5813f805dcb2f93
Sebastian <delucasd@gmail.com>
2021-01-27 10:34:02 -0300

    [TEC-4297] Adding changes to handle Flowcommerce Calculator and specs

---
+++ app/models/spree/calculator/flowcommerce_calculator.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module Spree
+  class Calculator
+    class FlowcommerceCalculator < Calculator::DefaultTax
+      def self.description
+        'FlowCommerce Calculator'
+      end
+
+      def compute_shipment_or_line_item(item)
+        order = item.order
+
+        if can_calculate_tax?(order)
+          flow_response = get_flow_response(order)
+          tax_for_item(item, flow_response)
+        else
+          prev_tax_amount(item)
+        end
+      end
+      alias compute_shipment compute_shipment_or_line_item
+      alias compute_line_item compute_shipment_or_line_item
+
+      private
+
+      def prev_tax_amount(item)
+        if rate.included_in_price
+          item.included_tax_total
+        else
+          item.additional_tax_total
+        end
+      end
+
+      def can_calculate_tax?(order)
+        return false if order.flow_data.blank?
+
+        true
+      end
+
+      def get_flow_response(order)
+        flow_io_tax_response = Rails.cache.fetch(order.flow_tax_cache_key, time_to_idle: 5.minutes) do
+          FlowcommerceSpree::Api.run :get, "/:organization/orders/allocations/#{order.number}"
+        end
+        flow_io_tax_response
+      end
+
+      def tax_for_item(item, flow_response)
+        prev_tax_amount = prev_tax_amount(item)
+        return prev_tax_amount if flow_response.nil?
+
+        item_details = flow_response['details']&.find do |el|
+          item.is_a?(Spree::LineItem) ? el['number'] == item.variant.sku : el['key'] == 'shipping'
+        end
+        details_key = rate.included_in_price ? 'included' : 'not_included'
+        price_components = item_details&.[](details_key)
+
+        amount = price_components&.find { |el| el['key'] == 'vat_item_price' }&.[]('total')&.[]('amount')
+        amount.present? && amount > 0 ? amount : prev_tax_amount
+      end
+    end
+  end
+end
---
+++ spec/support/flowcommerce_example_response.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+def flow_example_allocation(order_number, variant_sku, amount)
+  { 'id' => 'alc-5c6c3b5e10b44e4fb4063ec19fa10f65',
+    'order' => { 'id' => 'ord-550219e93efe4bfb8d4c32cf9e6d589e', 'number' => order_number, 'submitted_at' => '2021-01-27T18:29:04.266Z' },
+    'details' =>
+    [
+      { 'number' => variant_sku,
+        'quantity' => 1,
+        'key' => 'subtotal',
+        'price' => { 'currency' => 'EUR', 'amount' => 74.95, 'label' => '74,95 €', 'base' => { 'amount' => 89.46, 'currency' => 'USD', 'label' => 'US$89.46' } },
+        'total' => { 'currency' => 'EUR', 'amount' => 74.95, 'label' => '74,95 €', 'base' => { 'amount' => 89.46, 'currency' => 'USD', 'label' => 'US$89.46' } },
+        'included' =>
+      [{ 'key' => 'item_price',
+         'total' => { 'currency' => 'EUR', 'amount' => 62.98, 'label' => '62,98 €', 'base' => { 'amount' => 75.17, 'currency' => 'USD', 'label' => 'US$75.17' } },
+         'price' => { 'currency' => 'EUR', 'amount' => 62.98, 'label' => '62,98 €', 'base' => { 'amount' => 75.17, 'currency' => 'USD', 'label' => 'US$75.17' } },
+         'discriminator' => 'allocation_detail_component' },
+       { 'key' => 'rounding',
+         'total' => { 'currency' => 'EUR', 'amount' => 0, 'label' => '0,00 €', 'base' => { 'amount' => 0, 'currency' => 'USD', 'label' => 'US$0.00' } },
+         'price' => { 'currency' => 'EUR', 'amount' => 0, 'label' => '0,00 €', 'base' => { 'amount' => 0, 'currency' => 'USD', 'label' => 'US$0.00' } },
+         'discriminator' => 'allocation_detail_component' },
+       { 'key' => 'vat_item_price',
+         'total' => { 'currency' => 'EUR', 'amount' => amount, 'label' => '11,97 €', 'base' => { 'amount' => 14.29, 'currency' => 'USD', 'label' => 'US$14.29' } },
+         'rate' => 0.19,
+         'name' => 'VAT',
+         'accuracy' => 'calculated',
+         'price' => { 'currency' => 'EUR', 'amount' => amount, 'label' => '11,97 €', 'base' => { 'amount' => 14.29, 'currency' => 'USD', 'label' => 'US$14.29' } },
+         'discriminator' => 'allocation_levy_component' }],
+        'not_included' => [],
+        'id' => 'lin-c7e3f2fdf8b547538e6f29bc01827cad',
+        'discriminator' => 'allocation_line_detail' },
+      { 'key' => 'shipping',
+        'total' => { 'currency' => 'EUR', 'amount' => 283.46, 'label' => '283,46 €', 'base' => { 'amount' => 338.3, 'currency' => 'USD', 'label' => 'US$338.30' } },
+        'included' =>
+        [{ 'key' => 'shipping',
+           'total' => { 'currency' => 'EUR', 'amount' => 229.23, 'label' => '229.23 €', 'base' => { 'amount' => 338.3, 'currency' => 'USD', 'label' => 'US$338.30' } },
+           'price' => { 'currency' => 'EUR', 'amount' => 229.23, 'label' => '229.23 €', 'base' => { 'amount' => 338.3, 'currency' => 'USD', 'label' => 'US$338.30' } },
+           'discriminator' => 'allocation_detail_component' },
+         { 'key' => 'vat_item_price',
+           'total' => { 'currency' => 'EUR', 'amount' => amount, 'label' => '11,97 €', 'base' => { 'amount' => 14.29, 'currency' => 'USD', 'label' => 'US$14.29' } },
+           'rate' => 0.19,
+           'name' => 'VAT',
+           'accuracy' => 'calculated',
+           'price' => { 'currency' => 'EUR', 'amount' => amount, 'label' => '11,97 €', 'base' => { 'amount' => 14.29, 'currency' => 'USD', 'label' => 'US$14.29' } },
+           'discriminator' => 'allocation_levy_component' }],
+        'not_included' => [],
+        'discriminator' => 'allocation_order_detail' }
+    ],
+    'total' => { 'currency' => 'EUR', 'amount' => 358.41, 'label' => '358,41 €', 'base' => { 'amount' => 427.76, 'currency' => 'USD', 'label' => 'US$427.76' }, 'key' => 'localized_total' } }
+end
