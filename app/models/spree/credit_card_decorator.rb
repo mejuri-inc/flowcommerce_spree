f84b53acc3f172b6a341fcf5af94501eb1288c75
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2021-02-18 00:10:58 +0200

    [TEC-3791] Implemented `card_authorization_upserted_v2` webhook
    Changed Spree::CreditCardDecorator to a prepended Spree::FlowIoCreditCardDecorator module to ensure `flow_data` store_accessor will be working.
    
    Added Spree::FlowIoCreditCardDecorator.push_authorization method for storing cc authorizations in `card.meta`.

--- app/models/spree/credit_card_decorator.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Spree
-  CreditCard.class_eval do
-    serialize :meta, ActiveRecord::Coders::JSON.new(symbolize_keys: true)
-
-    store_accessor :meta, :flow_data
-  end
-end
