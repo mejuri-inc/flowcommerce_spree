d9a4d848c276dd53e8570ba4865fb68889b3d152
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2021-02-23 01:41:26 +0200

    [TEC-3791] Add meta.flow_data to Spree::PaymentCaptureEvent decorator to store capture id

--- db/migrate/20201021755957_add_meta_to_spree_tables.rb
+++ db/migrate/20201021755957_add_meta_to_spree_tables.rb
@@ -5,13 +5,15 @@ class AddMetaToSpreeTables < ActiveRecord::Migration
     add_column :spree_orders, :meta, :jsonb, default: '{}' unless column_exists?(:spree_orders, :meta)
     add_column :spree_promotions, :meta, :jsonb, default: '{}' unless column_exists?(:spree_promotions, :meta)
     add_column :spree_credit_cards, :meta, :jsonb, default: '{}' unless column_exists?(:spree_credit_cards, :meta)
+    add_column :spree_payment_capture_events, :meta, :jsonb, default: '{}' unless column_exists?(:spree_payment_capture_events, :meta)
   end
 
   def down
-    remove_column :spree_products, :meta if column_exists?(:spree_products, :meta)
-    remove_column :spree_variants, :meta if column_exists?(:spree_variants, :meta)
-    remove_column :spree_orders, :meta if column_exists?(:spree_orders, :meta)
-    remove_column :spree_promotions, :meta if column_exists?(:spree_promotions, :meta)
+    remove_column :spree_payment_capture_events, :meta if column_exists?(:spree_payment_capture_events, :meta)
     remove_column :spree_credit_cards, :meta if column_exists?(:spree_credit_cards, :meta)
+    remove_column :spree_promotions, :meta if column_exists?(:spree_promotions, :meta)
+    remove_column :spree_orders, :meta if column_exists?(:spree_orders, :meta)
+    remove_column :spree_variants, :meta if column_exists?(:spree_variants, :meta)
+    remove_column :spree_products, :meta if column_exists?(:spree_products, :meta)
   end
 end
