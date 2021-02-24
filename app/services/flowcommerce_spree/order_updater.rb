d7f3d30540bbf7cb17b4a6d3f1e83e6230dea15b
Sebastian <delucasd@gmail.com>
2021-02-24 10:53:49 -0300

    [TEC-4958] base changes to handle thank you page redirection

---
+++ app/services/flowcommerce_spree/order_updater.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module FlowcommerceSpree
+  class OrderUpdater
+    def initialize(order:)
+      raise(ArgumentError, 'Experience not defined or not active') unless order.zone&.flow_io_active_experience?
+
+      @experience = order.flow_io_experience_key
+      @order = order
+      @client = FlowcommerceSpree.client
+    end
+
+    def upsert_data(flow_io_order = nil)
+      flow_io_order ||= @client.orders.get_by_number(FlowcommerceSpree::ORGANIZATION, @order.number).to_hash
+
+      Rails.logger "[!] Flow IO Order data #{flow_io_order}"
+
+      @order.flow_data['order'] = flow_io_order
+      attrs_to_update = { meta: @order.meta.to_json }
+      if @order.flow_data.dig('order', 'submitted_at').present? && !@order.complete?
+        # flow_io_total_amount = order.flow_io_total_amount&.to_d
+        # attrs_to_update[:total] = flow_io_total_amount if flow_io_total_amount != order.total
+        # attrs_to_update[:updated_at] = Time.zone.now.utc
+
+        attrs_to_update[:email] = @order.flow_customer_email
+        # attrs_to_update[:state] = 'confirmed'
+        attrs_to_update[:payment_state] = 'pending'
+        attrs_to_update.merge!(@order.prepare_flow_addresses)
+        @order.state = 'delivery'
+        @order.save!
+        @order.create_proposed_shipments
+        @order.shipment.update_amounts
+        @order.line_items.each(&:store_ets)
+        @order.charge_taxes
+
+        # TODO : Add payment mapping
+      end
+
+      @order.update_columns(attrs_to_update)
+      puts 'Order Updated'
+      @order.save!
+    end
+
+    def finalize_order
+      @order.finalize!
+      @order.update_totals
+      @order.save
+      @order.after_completed_order
+    end
+
+    def complete_checkout
+      upsert_data
+
+      @order.state = 'complete'
+      @order.save
+
+      finalize_order
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@
 
 FlowcommerceSpree::Engine.routes.draw do
   post '/event-target', to: 'webhooks#handle_flow_web_hook_event'
+  get '/order-completed', to: 'orders#order_completed'
 end
