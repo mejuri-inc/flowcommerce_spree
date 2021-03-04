05078ff329405f865eefd1e2707d509c653422cd
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2021-03-04 20:35:49 +0200

    [TEC-4931] Completed the OrderSync.initialize method specs, adding test cases for initializing ivars and accessors

--- spec/services/flowcommerce_spree/order_sync_spec.rb
+++ spec/services/flowcommerce_spree/order_sync_spec.rb
@@ -28,7 +28,25 @@ RSpec.describe FlowcommerceSpree::OrderSync do
 
     context 'when the order has a flow experience defined' do
       let(:zone) { create(:germany_zone, :with_flow_data) }
-      let(:order) { create(:order_with_line_items, :with_flow_data) }
+      let(:order) { create(:order_with_line_items, :with_flow_data, zone: zone) }
+
+      it 'initializes the ivars and public accessors and calls the `fetch_session_id` method' do
+        allow(FlowcommerceSpree).to receive(:client).and_return('client instance')
+        allow_any_instance_of(FlowcommerceSpree::OrderSync).to receive(:fetch_session_id)
+        expect_any_instance_of(FlowcommerceSpree::OrderSync).to receive(:fetch_session_id)
+
+        instance = subject.new(order: order)
+
+        expect(instance.instance_variable_get(:@experience)).to eql(order.flow_io_experience_key)
+        expect(instance.instance_variable_get(:@order)).to eql(order)
+        expect(instance.instance_variable_get(:@client)).to eql('client instance')
+
+        expect(instance.respond_to?(:digest)).to be_truthy
+        expect(instance.respond_to?(:order)).to be_truthy
+        expect(instance.respond_to?(:response)).to be_truthy
+
+        expect(instance.order).to eql(order)
+      end
     end
   end
 end
