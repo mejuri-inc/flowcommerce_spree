45c9e0d2d3b5053ef4cf8fae9134d6fc98e2f824
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2021-03-04 16:45:02 +0200

    [TEC-4931] Fixed order not present case in OrderSync and added the test case

--- spec/services/flowcommerce_spree/order_sync_spec.rb
+++ spec/services/flowcommerce_spree/order_sync_spec.rb
@@ -5,9 +5,6 @@ require 'rails_helper'
 RSpec.describe FlowcommerceSpree::OrderSync do
   subject { FlowcommerceSpree::OrderSync }
 
-  let(:zone) { create(:germany_zone, :with_flow_data) }
-  let(:order) { create(:order_with_line_items, :with_flow_data) }
-
   describe '#initialize' do
     context 'when no order is passed' do
       it 'raises exception' do
@@ -15,15 +12,23 @@ RSpec.describe FlowcommerceSpree::OrderSync do
       end
     end
 
-    context 'when the order is not associated to flow experience' do
-      before do
-        allow(zone).to(receive(:flow_io_active_experience?).and_return(false))
-        allow(order).to(receive(:zone).and_return(zone))
+    context 'when order is not present' do
+      it 'raises exception' do
+        expect { subject.new(order: nil) }.to(raise_error.with_message('Experience not defined or not active'))
       end
+    end
+
+    context 'when the order has no flow experience' do
+      let(:order) { create(:order) }
 
       it 'raises exception' do
         expect { subject.new(order: order) }.to raise_error.with_message('Experience not defined or not active')
       end
+    end
+
+    context 'when the order has a flow experience defined' do
+      let(:zone) { create(:germany_zone, :with_flow_data) }
+      let(:order) { create(:order_with_line_items, :with_flow_data) }
     end
   end
 end
