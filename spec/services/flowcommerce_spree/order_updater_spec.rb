e48f84e4f12aad17fb8888b5d00ac904f17f122b
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2021-03-04 16:33:00 +0200

    [TEC-4931] Add test coverage to the gem

--- spec/services/flowcommerce_spree/order_updater_spec.rb
+++ spec/services/flowcommerce_spree/order_updater_spec.rb
@@ -3,113 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe FlowcommerceSpree::OrderUpdater do
-  subject { FlowcommerceSpree::OrderUpdater }
-
-  context 'when no order is passed' do
-    it 'raises exception' do
-      expect { subject.new }.to raise_error(ArgumentError, 'missing keyword: order')
-    end
-  end
+  let(:zone) { create(:germany_zone, :with_flow_data) }
+  let(:order) { create(:order_with_line_items, :with_flow_data) }
 
   context 'when order is not present' do
+    subject { FlowcommerceSpree::OrderUpdater.new(order: nil) }
+
     it 'raises exception' do
-      expect { subject.new(order: nil) }.to raise_error(ArgumentError, 'Experience not defined or not active')
+      expect { subject }.to(raise_error.with_message('Experience not defined or not active'))
     end
   end
 
   context 'when the order has no flow experience' do
     let(:order) { create(:order) }
 
+    subject { FlowcommerceSpree::OrderUpdater.new(order: order) }
+
     it 'raises exception' do
-      expect { subject.new(order: order) }.to raise_error(ArgumentError, 'Experience not defined or not active')
-    end
-  end
-
-  context 'when the order has a flow experience defined' do
-    let(:zone) { create(:germany_zone, :with_flow_data) }
-    let(:order) { create(:order_with_line_items, :with_flow_data, line_items_count: 2, zone: zone) }
-    let(:flowcommerce_client) { FlowcommerceSpree.client }
-
-    before { allow(FlowcommerceSpree).to receive(:client).and_return(flowcommerce_client) }
-
-    it 'initalizes successfully' do
-      instance = subject.new(order: order)
-
-      expect(instance.instance_variable_get(:@experience)).to eql(order.flow_io_experience_key)
-      expect(instance.instance_variable_get(:@order)).to eql(order)
-      expect(instance.instance_variable_get(:@client)).to eql(flowcommerce_client)
-    end
-
-    describe '#upsert_data' do
-      context 'if order status is `complete`' do
-        it 'does nothing' do
-          order.state = 'complete'
-          expect(order).not_to(receive(:create_proposed_shipments))
-          expect(order).not_to(receive(:charge_taxes))
-
-          subject.new(order: order).upsert_data
-          expect(order.complete?).to(be_truthy)
-        end
-      end
-
-      context 'if order status is not `complete`' do
-        before do
-          allow_any_instance_of(Io::Flow::V0::Clients::Orders)
-            .to(receive(:get_by_number)
-            .and_return({ submitted_at: Time.current, customer: { email: 'test@mejuri.com' } }))
-        end
-
-        it 'updates order to state `payment` and calls several methods to update the order related records' do
-          expect(order).to(receive(:create_proposed_shipments))
-          order.line_items.each { |item| expect(item).to(receive(:store_ets).exactly(1).times) }
-          expect(order).to(receive(:charge_taxes))
-
-          subject.new(order: order).upsert_data
-          expect(order.payment?).to(be_truthy)
-        end
-      end
-    end
-
-    describe '#finalize_order' do
-      before do
-        allow_any_instance_of(FlowcommerceSpree::OrderSync).to receive(:fetch_session_id)
-        allow_any_instance_of(FlowcommerceSpree::OrderSync).to receive(:synchronize!)
-      end
-
-      it 'Sets order as completed with completed_at filled' do
-        expect(order).to(receive(:finalize!)).and_call_original
-        expect(order).to(receive(:update_totals)).and_call_original
-        expect(order).to(receive(:after_completed_order)).and_call_original
-
-        subject.new(order: order).finalize_order
-
-        expect(order.completed_at).to(be_present)
-      end
-    end
-
-    describe '#complete_checkout' do
-      before do
-        expect_any_instance_of(FlowcommerceSpree::OrderUpdater).to(receive(:upsert_data))
-        expect_any_instance_of(FlowcommerceSpree::OrderUpdater).to(receive(:map_payments_to_spree)).and_call_original
-      end
-
-      context 'when payments amount is equal or greater than order`s amount' do
-        before do
-          allow(order).to(receive(:amount).and_return(order.payments.sum(:amount)))
-        end
-
-        it 'calls finalize_order method' do
-          expect_any_instance_of(FlowcommerceSpree::OrderUpdater).to(receive(:finalize_order))
-          subject.new(order: order).complete_checkout
-        end
-      end
-
-      context 'when payments amount is less than order`s amount' do
-        it 'does not call finalize_order if order is not updated to`complete`' do
-          expect_any_instance_of(FlowcommerceSpree::OrderUpdater).not_to(receive(:finalize_order))
-          subject.new(order: order).complete_checkout
-        end
-      end
+      expect { subject }.to(raise_error.with_message('Experience not defined or not active'))
     end
   end
 end
