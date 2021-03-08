00be1917d5743f61f8de84161fb7f1bb8669e0a0
Sebastian <delucasd@gmail.com>
2021-03-08 16:47:30 -0300

    [TEC-3823] Changing from GET to POST endpoint flow/online-stock-availability

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@
 FlowcommerceSpree::Engine.routes.draw do
   post '/event-target', to: 'webhooks#handle_flow_web_hook_event'
   get '/order-completed', to: 'orders#order_completed'
-  get '/online-stock-availability', to: 'inventory#online_stock_availability'
+  post '/online-stock-availability', to: 'inventory#online_stock_availability'
 end
