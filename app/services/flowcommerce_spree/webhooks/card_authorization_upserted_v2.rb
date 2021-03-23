62e9b13811b0077ed53d3284393de5555d23a22e
Aurel Branzeanu <branzeanu.aurel@gmail.com>
2021-03-23 20:26:18 +0200

    [DL-77] card_authorization_upserted_v2 service object decomposition and simplification refactoring

--- app/services/flowcommerce_spree/webhooks/card_authorization_upserted_v2.rb
+++ app/services/flowcommerce_spree/webhooks/card_authorization_upserted_v2.rb
@@ -11,39 +11,22 @@ module FlowcommerceSpree
       end
 
       def initialize(data, opts = {})
-        @data = data
+        @card_auth = data['authorization']&.to_hash
+        @card_auth['method'].delete('images')
         @opts = opts
         @errors = []
       end
 
       def process
-        card_auth = @data['authorization']&.to_hash
-        errors << { message: 'Authorization param missing' } && (return self) unless card_auth
+        errors << { message: 'Authorization param missing' } && (return self) unless @card_auth
 
-        errors << { message: 'Card param missing' } && (return self) unless (flow_io_card = card_auth.delete('card'))
+        errors << { message: 'Card param missing' } && (return self) unless (flow_io_card = @card_auth.delete('card'))
 
-        if (order_number = card_auth.dig('order', 'number'))
+        if (order_number = @card_auth.dig('order', 'number'))
           if (order = Spree::Order.find_by(number: order_number))
-            flow_io_card_expiration = flow_io_card.delete('expiration')
+            card = upsert_card(flow_io_card, order)
 
-            card = Spree::CreditCard.find_or_initialize_by(month: flow_io_card_expiration['month'].to_s,
-                                                           year: flow_io_card_expiration['year'].to_s,
-                                                           cc_type: flow_io_card.delete('type'),
-                                                           last_digits: flow_io_card.delete('last4'),
-                                                           name: flow_io_card.delete('name'),
-                                                           user_id: order.user&.id)
-            card.flow_data ||= {}
-            card.flow_data.merge!(flow_io_card.except('discriminator')) if card.new_record?
-            card_auth['method'].delete('images')
-            card.push_authorization(card_auth.except('discriminator'))
-            if card.new_record?
-              card.imported = true
-              card.save!
-            else
-              card.update_column(:meta, card.meta.to_json)
-            end
-
-            order.payments.where(response_code: card_auth['id'])
+            order.payments.where(response_code: @card_auth['id'])
                  .update_all(source_id: card.id, source_type: 'Spree::CreditCard')
 
             return card
@@ -55,6 +38,29 @@ module FlowcommerceSpree
         end
 
         self
+      end
+
+      private
+
+      def upsert_card(flow_io_card, order)
+        flow_io_card_expiration = flow_io_card.delete('expiration')
+
+        card = Spree::CreditCard.find_or_initialize_by(month: flow_io_card_expiration['month'].to_s,
+                                                       year: flow_io_card_expiration['year'].to_s,
+                                                       cc_type: flow_io_card.delete('type'),
+                                                       last_digits: flow_io_card.delete('last4'),
+                                                       name: flow_io_card.delete('name'),
+                                                       user_id: order.user&.id)
+        card.flow_data ||= {}
+        if card.new_record?
+          card.flow_data.merge!(flow_io_card.except('discriminator'))
+          card.imported = true
+        end
+
+        card.push_authorization(@card_auth.except('discriminator'))
+        card.new_record? ? card.save : card.update_column(:meta, card.meta.to_json)
+
+        card
       end
     end
   end
