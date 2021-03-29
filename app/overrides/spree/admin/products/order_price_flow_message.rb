49bb62218af8ba0b812a768662b7e57d4aa8fc0c
Sebastian <delucasd@gmail.com>
2021-03-29 12:05:49 -0300

    [DL-72] Updating price message css to be nicer

--- app/overrides/spree/admin/products/order_price_flow_message.rb
+++ app/overrides/spree/admin/products/order_price_flow_message.rb
@@ -3,9 +3,7 @@ Deface::Override.new(
   name: 'spree_admin_prices_flow_mesage',
   insert_top: '.no-border-top',
   text: "
-  <div style='margin-bottom: 20px;'>
-    <h5>
-      Some prices might not be shown in this list. You can find them here <a href='#{"https://console.flow.io/#{ENV['FLOW_ORGANIZATION']}/price-books"}' target='_blank'>here</a>.
-    </h5>
+  <div class='spree-admin-info' >
+    Some prices might not be shown in this list. You can find them <a href='#{"https://console.flow.io/#{ENV['FLOW_ORGANIZATION']}/price-books"}' target='_blank'>here</a>.
   </div>"
 )
